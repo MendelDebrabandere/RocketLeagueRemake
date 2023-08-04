01cb3996d26c0aed2833744de03d18fc9b41ecc5
Mendel Debrabandere <mendel.debrabandere@hotmail.com>
2023-08-04 15:44:41 +0200

    Initial commit

---
+++ Source/RLRemake/RLRemakeGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RLRemakeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RLREMAKE_API ARLRemakeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
